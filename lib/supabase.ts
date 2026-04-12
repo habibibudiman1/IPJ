5851f476816daf93d4a5baa5fe0f8c667607eee9
habibibudiman1 <habibibudiman@student.telkomuniversity.ac.id>
2026-04-13 01:34:11 +0700

    perbaikan supabase

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "https://placeholder.supabase.co";
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "placeholder_key";
+const supabaseUrl =
+  process.env.NEXT_PUBLIC_SUPABASE_URL ||
+  "https://zlvojhdxlmsxucszktzb.supabase.co";
+const supabaseAnonKey =
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpsdm9qaGR4bG1zeHVjc3prdHpiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk3MDMxMjgsImV4cCI6MjA4NTI3OTEyOH0.DDWou0_xC-SWDwuRx5XmXayWOyBk3XYtaf7jaShVsYU";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
